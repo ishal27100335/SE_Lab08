7d5778fa3b75ea42ff6244b48b2f77b235760d4f
ishal27100335 <27100335@lums.edu.pk>
2026-03-19 04:12:44 +0500

    Implemented deleteCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -14,7 +14,9 @@ public class CustomList {
         cities.add(city);
     }
 
-    
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
 
     // Will be implemented later using TDD (leave blank for now)
     public boolean hasCity(City city) {
